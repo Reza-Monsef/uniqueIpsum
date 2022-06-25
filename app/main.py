3a94db99d043b836216bb8d46bee92157eabb6f7
Your Name <you@example.com>
2022-06-25 09:09:40 -0700

    update

--- app/main.py
+++ app/main.py
@@ -1,7 +1,26 @@
 from  fastapi import FastAPI
+from  bs4 import BeautifulSoup
+import requests
 
 app = FastAPI()
 
 @app.get('/')
 def Home():
     return {"message":"welcome"}
+
+@app.post('/ipsum/q')
+def IpsumSearchView(text: str):
+    req = requests.get(f"https://en.wikipedia.org/wiki/{text}")
+    req_decode = req.content.decode()
+    soup  = BeautifulSoup(req_decode, "html.parser")
+    p_tags = soup.find_all('p')
+    response = ''
+    counter = 0
+    for item in p_tags:
+        if counter < 3:
+            if len(item.text) > 100:
+                response += item.text
+                counter += 1
+        else:
+            break
+    return {"succeeded": True, "result": response}
